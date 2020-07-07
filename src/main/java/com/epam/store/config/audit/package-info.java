715c03fcb06d7798edf488ef7cd61688225dd35d
freelancing1552 <freelancing1552@gmail.com>
2020-07-07 12:15:50 +0530

    Initial version of store generated by JHipster-6.8.0

---
+++ src/main/java/com/epam/store/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.epam.store.config.audit;
---
+++ src/main/java/com/epam/store/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.epam.store.repository;
+
+import com.epam.store.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
